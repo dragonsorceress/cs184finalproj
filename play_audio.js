3a1b9930295252d28d1a00eda3cbf5ab86bae311
Jarry Xiao <jarry.xiao@berkeley.edu>
2018-04-15 17:57:28 -0700

    added started fft code

--- play_audio.js
+++ play_audio.js
@@ -11,25 +11,42 @@ var right_freqs = [];
 request.open("GET", url, true);
 request.responseType = "arraybuffer";
 request.onload = function() {
-    console.log("here");
     console.log(request.response);
-
-    context.decodeAudioData(request.response, function(buffer){
-        left = buffer.getChannelData(0);
-        right = buffer.getChannelData(1);
-        do_stuff(left, do_next_stuff)
-        console.log(left);
-        console.log(right);
-        }, function(){
-        onError();
-    });
+    context.decodeAudioData(request.response, 
+        function(buffer){
+            left = buffer.getChannelData(0);
+            right = buffer.getChannelData(1);
+            left_freq = fft(left);
+            console.log(left_freq);
+            console.log(left);
+            console.log(right);
+        }, 
+        function(){
+            onError();
+        }
+    );
 }
 request.send()
 
-function fft(buffer, callback) {
+function fft(buffer, size=512) {    
+    var spectrums = [];
+    for (var i = 0; i < buffer.length; i += size) {
 
+        var segment = buffer.slice(i, i+size);
+        if (i+size > buffer.length) {
+            var zeros = new Float32Array(i+size-buffer.length).fill(0.0);
+            var first = segment.length;
+            var result = new Float32Array(512);
+            result.set(segment);
+            result.set(zeros, first);
+            segment = result;
+        }
+        var spectrum = new FFT(512, 44100);
+        var out = spectrum.forward(segment);
+        console.log(out);
+        spectrums.push(out);
+    } 
+    return spectrums;
 }
-
-function do_next_stuff()
 
 
