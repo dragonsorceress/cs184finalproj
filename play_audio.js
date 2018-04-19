739fe03b34723af553aa4d6bfb94a07c5b0dcb7f
Jarry Xiao <jarry.xiao@berkeley.edu>
2018-04-19 15:10:38 -0700

    added some comments

--- play_audio.js
+++ play_audio.js
@@ -17,38 +17,54 @@ function createAudioGraph(buffer, context) {
     var source = null
         startedAt = 0,
         pausedAt = 0,
-        playing = false;
-
-    var scriptNode = context.createScriptProcessor(2048, 2, 2);
-
+        playing = false, 
+        scriptNode = null;
     var play = function() {
+        // Initializes node to stream in audio data in chunks
+        scriptNode = context.createScriptProcessor(2048, 2, 2);
         scriptNode.buffer = buffer;
         scriptNode.connect(context.destination);
 
+        // Creates node to analyze data from the left channel
         var left_analyser = context.createAnalyser();
         left_analyser.smoothingTimeConstant = 0.6;
         left_analyser.fftSize = 512;
 
+        // Creates node to analyze data from the left channel
         var right_analyser = context.createAnalyser();
         right_analyser.smoothingTimeConstant = 0.6;
         right_analyser.fftSize = 512;
 
+        // Initializes source node to play the audio
         source = context.createBufferSource();
         source.buffer = buffer;
         source.loop = true;
 
+        // Creates a splitter node to process multiple channels of audio
         splitter = context.createChannelSplitter(2);
 
+        // Splits audio data into 2 channels
         source.connect(splitter);
 
+        // Each channel is connected to its own analyzer
         splitter.connect(left_analyser, 0);
         splitter.connect(right_analyser, 1);
 
+        // Each analyzer is connected to the script node to receive data
         left_analyser.connect(scriptNode);
         right_analyser.connect(scriptNode);
 
+        // The source is connected to the audio file
         source.connect(context.destination);
 
+        /* 
+           Here is where the magic happens. We can now pull in frequency data from
+           both channels and do stuff with these arrays. Each array contains the
+           magnitudes of the coefficients of the first 256 frequency components of the 
+           size 512 DFT. The remaining 256 components are just the reverse of the computed 
+           coefficients, as they form complex conjagates with the first 256 coefficients
+           (same magnitude).
+        */
         scriptNode.onaudioprocess = function(e) {
             var left = new Float32Array(left_analyser.frequencyBinCount);
             var right = new Float32Array(right_analyser.frequencyBinCount);
@@ -62,9 +78,9 @@ function createAudioGraph(buffer, context) {
         startedAt = context.currentTime - offset;
         pausedAt = 0;
         playing = true;
-
         source.start(0, offset);
     }
+
     var pause = function() {
         var elapsed = context.currentTime - startedAt;
         stop();
@@ -110,16 +126,13 @@ function createAudioGraph(buffer, context) {
         pause: pause,
         stop: stop
     };
-
 }
 
 function init(buffer) {
     var graph = createAudioGraph(buffer, context);
-
     var play = document.querySelector('[data-js="play"]'),
-    stop = document.querySelector('[data-js="stop"]'),
-    info = document.querySelector('[data-js="info"]');
-
+        stop = document.querySelector('[data-js="stop"]'),
+        info = document.querySelector('[data-js="info"]');
     play.addEventListener('click', function() {
         if (graph.getPlaying()) {
             graph.pause();
@@ -134,7 +147,6 @@ function init(buffer) {
         play.innerHTML = 'play';
     });
 };
-
 
 function error(e){
     console.error('ERROR: context.decodeAudioData:', e);
